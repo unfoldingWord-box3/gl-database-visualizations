82e6e9760464ecb46aa914e76806526f9c520ff4
B Alexander <13680864+binu-alexander@users.noreply.github.com>
2023-06-13 14:57:57 +0530

    Update

--- Python/PolyMaxMinDistance.py
+++ Python/PolyMaxMinDistance.py
@@ -1,84 +1,75 @@
-import numpy as np
-from qgis.core import (QgsVectorLayer, QgsFeature, QgsField, QgsPointXY, QgsGeometry, QgsVectorFileWriter, Qgis)
-from qgis.PyQt.QtCore import QVariant
+import geopandas as gpd
+from shapely.geometry import Polygon, Point
+import random
+import csv
 
-# Assuming layer is your polygon layer
-layer = iface.activeLayer()
+# Load your polygon layer into a GeoDataFrame
+polygon_gdf = gpd.read_file('polygons.gpkg')
 
-def get_points_per_unit(area):
-    # Adjust these values to fit your needs
-    if area < 100:
-        return 100
-    elif area < 1000:
-        return 200
-    else:
-        return 300
+# Reproject the GeoDataFrame to EPSG:7755
+polygon_gdf = polygon_gdf.to_crs(epsg=7755)
 
-print("Creating a copy of the original layer...")
-crs = layer.crs().toWkt()
-layer_copy = QgsVectorLayer('Polygon?crs=' + crs, 'layer_copy', 'memory')
+# Create a new column to store the maximum possible minimum distance
+polygon_gdf['max_min_distance'] = None
 
-print("Starting to edit the copy...")
-layer_copy.startEditing()
-
-print("Copying the attributes of the original layer...")
-layer_copy_data = layer_copy.dataProvider()
-for field in layer.fields():
-    layer_copy_data.addAttributes([field])
-layer_copy.updateFields()
-
-print("Adding new attributes for the max min distance...")
-layer_copy_data.addAttributes([QgsField("MaxMinDistance", QVariant.Double)])
-layer_copy.updateFields()
-
-print("Iterating over the original layer...")
-for feature in layer.getFeatures():
-    polygon = feature.geometry()
-    bbox = polygon.boundingBox()
-
-    points_per_unit = get_points_per_unit(polygon.area())
-    num_points = int(polygon.area() * points_per_unit)
+# Iterate over each polygon and calculate the maximum possible minimum distance
+for index, row in polygon_gdf.iterrows():
+    polygon = row['geometry']
+    
+    # Fix invalid geometries
+    if not polygon.is_valid:
+        polygon = polygon.buffer(0)
+    
+    # Calculate the size-based density factor for random points
+    density_factor = polygon.length / polygon.area
+    
+    # Set a minimum number of points for smaller polygons
+    min_num_points = 100  # Adjust as needed
+    
+    # Adjust the number of random points based on polygon size and desired density
+    num_points = max(int(polygon.area * density_factor / 1000), min_num_points)
+    
+    # Generate random points within the polygon
     points = []
+    bbox = polygon.bounds  # Get the bounding box of the polygon
+    min_x, min_y, max_x, max_y = bbox
+    max_attempts = 1000  # Maximum number of attempts to generate points
+    attempts = 0
+    while len(points) < num_points and attempts < max_attempts:
+        rand_x = random.uniform(min_x, max_x)
+        rand_y = random.uniform(min_y, max_y)
+        point = Point(rand_x, rand_y)
+        if polygon.contains(point):
+            points.append(point)
+        attempts += 1
+    
+    # Calculate the minimum distance from each point to the border
+    min_distances = [polygon.boundary.distance(point) for point in points]
+    
+    # Check if the min_distances list is empty
+    if min_distances:
+        # Find the maximum of the minimum distances and convert it to kilometers
+        max_min_distance = max(min_distances) / 1000
+    else:
+        max_min_distance = 0  # Set a default value if no points are within the polygon
+    
+    # Store the maximum possible minimum distance in the 'max_min_distance' column
+    polygon_gdf.at[index, 'max_min_distance'] = max_min_distance
 
-    print(f"Generating points within the bounding box...")
-    while not points:
-        for _ in range(num_points):
-            i = np.random.uniform(bbox.xMinimum(), bbox.xMaximum())
-            j = np.random.uniform(bbox.yMinimum(), bbox.yMaximum())
-            point = QgsPointXY(i, j)
-            geom_point = QgsGeometry.fromPointXY(point)
+# Create a CSV file to store the results
+output_file = 'polygon_distances.csv'
 
-            if polygon.contains(geom_point): 
-                points.append(geom_point)
-        if not points:
-            print("No points were generated within the polygon. Trying again...")
+# Open the CSV file in write mode and write the header
+with open(output_file, 'w', newline='') as csvfile:
+    fieldnames = ['ISO', 'Polygon Index', 'Max Min Distance']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    
+    # Write the ISO, polygon index, and corresponding maximum possible minimum distance to each row
+    for index, row in polygon_gdf.iterrows():
+        iso = row['iso']
+        polygon_index = index
+        max_min_distance = row['max_min_distance']
+        writer.writerow({'ISO': iso, 'Polygon Index': polygon_index, 'Max Min Distance': max_min_distance})
 
-    max_min_distance = 0
-
-    print("Computing the minimum distance from each point to the polygon...")
-    for point in points:
-        min_distance = polygon.distance(point)
-
-        if min_distance > max_min_distance:
-            max_min_distance = min_distance
-
-    print("Copying the feature from the original layer...")
-    new_feature = QgsFeature(layer_copy.fields())
-    new_feature.setGeometry(feature.geometry())
-    for field in feature.fields():
-        try:
-            new_feature[field.name()] = feature[field.name()]
-        except Exception as e:
-            print(f"Error when copying attribute {field.name()}: {str(e)}")
-
-    print("Setting the new attributes...")
-    new_feature["MaxMinDistance"] = max_min_distance
-
-    print("Adding the new feature to the copy layer...")
-    layer_copy.addFeature(new_feature)
-
-print("Committing the changes and adding the new layer to the map...")
-layer_copy.commitChanges()
-QgsProject.instance().addMapLayer(layer_copy)
-
-print('New layer with results added to the map.')
+print(f"CSV file '{output_file}' created successfully.")
