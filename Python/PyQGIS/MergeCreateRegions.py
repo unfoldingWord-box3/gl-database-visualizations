a270c61ee0a1e2bc9f05e306386e5012a8c248df
B Alexander <13680864+binu-alexander@users.noreply.github.com>
2023-06-29 13:26:00 -0700

    update

--- Python/PyQGIS/MergeCreateRegions.py
+++ Python/PyQGIS/MergeCreateRegions.py
@@ -1,24 +1,20 @@
 import processing
+layer = iface.activeLayer()  # Assuming you are running this script within the QGIS Python console
 
-# Get the active layer (assuming this script is run within the QGIS Python console)
-layer = iface.activeLayer()
+field_name = 'join_Region'  # Replace 'join_Region' with your desired field name
 
-# Define the field name (replace 'join_Region' with your desired field name)
-field_name = 'join_Region'
-
-# Set the parameters for the dissolve operation
 parameters = {
     'INPUT': layer,
     'FIELD': [field_name],
     'SEPARATE_DISJOINT': False,
-    'OUTPUT': 'TEMPORARY_OUTPUT'
+    'OUTPUT': 'TEMPORARY_OUTPUT'  # Output set to 'TEMPORARY_OUTPUT' to create a temporary layer
 }
 
-# Run the dissolve operation
 result = processing.run("native:dissolve", parameters)
 
-# Check if the dissolve operation was successful
 if result['OUTPUT']:
-    print("Dissolve operation completed successfully.")
+    dissolved_layer = result['OUTPUT']  # Access the temporary layer from the result
+    QgsProject.instance().addMapLayer(dissolved_layer)  # Add the temporary layer to the map
+    print("Dissolve operation completed successfully. Temporary layer added to the map.")
 else:
     print("Dissolve operation failed.")
